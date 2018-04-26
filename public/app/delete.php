b9200d199cecc72d10188d065862f7d46c9ddf8c
Unknown <d251062@edu.rocwb.nl>
2018-04-26 09:17:59 +0200

    Update all files to current state

--- public/app/delete.php
+++ public/app/delete.php
@@ -3,6 +3,8 @@
 require("databaseconnector.php");
 
 if (isset($_POST['delete'])){
-    $deletequery = "DELETE FROM `tbl_teams`";
+    $name = $_POST['delete'];
+    $deletequery = "DELETE FROM `tbl_teams` WHERE `name`= '$name'";
     $database->query($deletequery);
 }
+header("Location:../public/invoer-teams.php");
